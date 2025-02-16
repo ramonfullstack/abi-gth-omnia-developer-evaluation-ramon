2dd001a16f4ff0c819060aa954c929d1ee1f49da
Ramon Silva <ramon.santos+magna@partner.magna.com>
2025-02-16 20:48:30 -0300

    feat: created updated sale events and command

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/UpdateSale/UpdateSaleResult.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// Represents the response returned after successfully updating a sale.
+/// </summary>
+/// <remarks>
+/// This response contains the unique identifier of the sale updated,
+/// which can be used for subsequent operations or reference.
+/// </remarks>
+public class UpdateSaleResult
+{
+    /// <summary>
+    /// Gets or sets the unique identifier of the newly created sale.
+    /// </summary>
+    /// <value>A GUID that uniquely identifies the created sale in the system.</value>
+    public Guid Id { get; set; }
+}
