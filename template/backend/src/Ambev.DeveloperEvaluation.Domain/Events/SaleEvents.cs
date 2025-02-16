9270ce8a4db6dd644ed9f41f60746efdd77b48c9
Ramon Silva <ramon.santos+magna@partner.magna.com>
2025-02-16 20:28:11 -0300

    feat: created sale events

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Events/SaleEvents.cs
@@ -0,0 +1,6 @@
+﻿namespace Ambev.DeveloperEvaluation.Domain.Events
+{
+    public record SaleCreatedEvent(Guid SaleId, string Customer, decimal TotalAmount);
+    public record SaleUpdatedEvent(Guid SaleId, string Customer, decimal TotalAmount, bool IsCanceled);
+    public record SaleDeletedEvent(Guid SaleId);
+}
