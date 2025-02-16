99447d8150ca181bbcc1f35c2489b133ca194faa
Ramon Silva <ramon.santos+magna@partner.magna.com>
2025-02-16 20:43:48 -0300

    feat: created event delete

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/DeleteSale/DeleteSaleCommand.cs
@@ -0,0 +1,22 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.DeleteSale;
+using MediatR;
+
+/// <summary>
+/// Command for deleting a sale
+/// </summary>
+public record DeleteSaleCommand : IRequest<DeleteSaleResponse>
+{
+    /// <summary>
+    /// The unique identifier of the sale to delete
+    /// </summary>
+    public Guid Id { get; }
+
+    /// <summary>
+    /// Initializes a new instance of DeleteSaleCommand
+    /// </summary>
+    /// <param name="id">The ID of the sale to delete</param>
+    public DeleteSaleCommand(Guid id)
+    {
+        Id = id;
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/DeleteSale/DeleteSaleResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.DeleteSale
+{
+    /// <summary>
+    /// Response model for DeleteSale operation
+    /// </summary>
+    public class DeleteSaleResponse
+    {
+        /// <summary>
+        /// Indicates whether the deletion was successful
+        /// </summary>
+        public bool Success { get; set; }
+    }
+
+}
