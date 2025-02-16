25e285a72444970efc68a378f92c4261444a990a
Ramon Silva <ramon.santos+magna@partner.magna.com>
2025-02-16 20:30:49 -0300

    feat: created event publisher

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Eventing/EventPublisher.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Ambev.DeveloperEvaluation.Application.Eventing
+{
+    public class EventPublisher : IEventPublisher
+    {
+        private readonly ILogger<EventPublisher> _logger;
+
+        public EventPublisher(ILogger<EventPublisher> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task PublishAsync<TEvent>(TEvent @event, CancellationToken cancellationToken) where TEvent : class
+        {
+            _logger.LogInformation("Event published: {EventName}, Data: {@EventData}", @event.GetType().Name, @event);
+
+            await Task.CompletedTask;
+        }
+    }
+}
